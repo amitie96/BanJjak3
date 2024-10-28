549b33858e3c06ec4cf9b57f1f311f6cdd57d9e9
amitie96 <tkd369@naver.com>
2024-10-28 20:37:12 +0900

    10/28 공지사항 수정기능 추가 완

--- banzzak/src/main/java/com/mysite/banzzak/notice/NoticeController.java
+++ banzzak/src/main/java/com/mysite/banzzak/notice/NoticeController.java
@@ -6,10 +6,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.naming.Binding;
+import java.security.Principal;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -29,6 +31,7 @@ public class NoticeController {
     public String noticeCreate(NoticeForm noticeForm){
         return "notice/notice_form";
     }
+
     @PostMapping("/create")
     public String noticeCreate(@Valid NoticeForm noticeForm, BindingResult bindingResult){
         if(bindingResult.hasErrors()){
@@ -36,5 +39,22 @@ public class NoticeController {
         }
         this.noticeService.create(noticeForm.getNoticeTitle(),noticeForm.getNoticeContent());
         return "redirect:/notice/list";
+    }
+
+    @GetMapping("/detail/{noticeId}")
+    public String detail(Model model, @PathVariable("noticeId") Integer noticeId){
+    Notice notice = (Notice) this.noticeService.getNotice(noticeId);
+    model.addAttribute("notice",notice);
+        return "notice/notice_detail";
+    }
+    @PostMapping("/detail/{noticeId}")
+    public String edit(@Valid NoticeForm noticeForm, BindingResult bindingResult,
+                       Principal principal, @PathVariable("noticeId") Integer noticeId){
+        if (bindingResult.hasErrors()) {
+            return "notice/notice_detail"; // 오류가 있을 경우 수정 화면으로 돌아감
+        }
+        Notice notice = this.noticeService.getNotice(noticeId);
+        this.noticeService.edit(notice, noticeForm.getNoticeTitle(), noticeForm.getNoticeContent());
+        return String.format("redirect:/notice/detail/%d",noticeId);
     }
 }
--- banzzak/src/main/java/com/mysite/banzzak/notice/NoticeService.java
+++ banzzak/src/main/java/com/mysite/banzzak/notice/NoticeService.java
@@ -1,10 +1,12 @@
 package com.mysite.banzzak.notice;
 
+import com.mysite.banzzak.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -15,11 +17,26 @@ public class NoticeService {
         return this.noticeRepository.findAll();
     }
 
-    public void create(String Noticetitle, String Noticecontent) {
+  public void create(String noticeTitle, String noticeContent) {
         Notice n = new Notice();
-        n.setNoticeTitle(Noticetitle);
-        n.setNoticeContent(Noticecontent);
+        n.setNoticeTitle(noticeTitle);
+        n.setNoticeContent(noticeContent);
         n.setNoticeDate(LocalDateTime.now());
         this.noticeRepository.save(n);
     }
+    public Notice getNotice(Integer noticeId){
+        Optional<Notice> notice = this.noticeRepository.findById(noticeId);
+        if(notice.isPresent()){
+            return notice.get();
+        }else{
+            throw new DataNotFoundException("notice not found");
+        }
+    }
+    public void edit(Notice notice, String noticeTitle, String noticeContent){
+        notice.setNoticeTitle(noticeTitle);
+        notice.setNoticeContent(noticeContent);
+        this.noticeRepository.save(notice);
+
+    }
+
 }
